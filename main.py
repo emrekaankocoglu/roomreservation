0985235cce5c3286ae683d70bbd10a4d9a5b3862
furkan-aksakal <85026220+furkan-aksakal@users.noreply.github.com>
2023-11-22 23:07:46 +0300

    devamı gelecek

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ from view.view import View
     
 """
 
-
+def createOrganization():
+    pass
 def createEvent(user):
     title = "Sample Event"
     description = "This is a sample event description."
@@ -43,34 +44,83 @@ def createRoom(user):
     room = Room.create(name,x,y, capacity, working_hours, permissions)
     return room
 
+def retrieveOrganization(id):
+    organization = Organization.retrieve(id)
+    return organization
+def retrieveEvent(id):
+    event = Event.retrieve(id)
+    return event
+def retrieveRoom(id):
+    room = Room.retrieve(id)
+    return room
 
+def updateOrganization(id,update_dict):
+    organization = Organization.update(id,update_dict)
+    return organization
+def updateEvent(id, **kwargs):
+    event = Event.update(id, **kwargs)
+    return event
+def updateRoom(id, **kwargs):
+    room = Room.update(id, **kwargs)
+    return room
 
-def updateOrganization():
-    pass
-def updateEvent():
-    pass
-def updateRoom():
-    pass
-
-def deleteOrganization():
-    pass
-def deleteEvent():
-    pass
-def deleteRoom():
-    pass
+def deleteOrganization(id):
+    Organization.delete(id)
+def deleteEvent(id):
+    Event.delete(id)
+def deleteRoom(id):
+    Room.delete(id)
 
 def createUser():
-    user = User.createUser("x","y","z","w","s",)
+    user = User.createUser(0,"y","z","w","s",)
     return user
 
-def switchUser():
-    pass
+
 
 def main():
+    #isSingleton
     cat = Catalogue()
+    print(cat)
+    cat2 = Catalogue()
+    print(cat2)
+    
+    #switch user work
     user = createUser()
+    user2 = User.createUser(1,"b","c","d","f")
     cat.switchuser(user)
-    a = createRoom(user.id)
+    print(cat.switchuser(user))
+    cat.switchuser(user2)
+    print(cat.switchuser(user2))
+    
+    #crud on room
+    a = createRoom(user2.id)
+    print(a)
+    aRet = retrieveRoom(a.id)
+    print(aRet)
+    aUp = updateRoom(a, **{'x':10})
+    print(aUp.x)
+    deleteRoom(a.id)
+    
+
+    #crud on event
+    b = createEvent(user2.id)
+    print(b)
+    bRet = retrieveEvent(b.id)
+    print(bRet)
+    bUp = updateEvent(b, **{'title': 'Not Sample Event'})
+    print(bUp)
+    deleteEvent(b.id)
+    
+    #methods on organization
+    org = Organization.create(user2.id,"IEEE",None,{},{})
+    print(org)
+    org.registerRoom(a)
+    org.registerEvent(b)
+    print(org)
+    org.reserve(b,a,datetime.now()+timedelta(days=3)) 
+    org.findSchedule([v for v in org.events.values()], ((0,0), (100,100)), datetime.now(), datetime.now()+timedelta(days=2))
+    
+    """
     b = createEvent(user.id)
     c = Organization.create(user.id,"IEEE",None,{},{})
     c.registerRoom(a)
@@ -96,6 +146,7 @@ def main():
     user.view.dayView(datetime.now(), datetime.now()+timedelta(days=2))
     user.view.roomView(datetime.now(), datetime.now()+timedelta(days=2))
     user.view.delquery(1)
+    """
 
 if __name__=="__main__": 
     main() 
